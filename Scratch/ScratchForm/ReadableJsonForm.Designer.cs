87d8902cecacda80c1ffbdec9fb58db689ae17fc
Marty Thompson <martythompson@gmail.com>
2016-07-21 21:43:29 +0000

    Initial Commit
    git-tfs-id: [https://ytram-tfs.visualstudio.com]$/Scratch;C8

---
+++ Scratch/ScratchForm/ReadableJsonForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ScratchForm
+{
+    partial class ReadableJsonForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.fileOpener = new System.Windows.Forms.OpenFileDialog();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOutput.Location = new System.Drawing.Point(642, 12);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(122, 23);
+            this.btnOutput.TabIndex = 0;
+            this.btnOutput.Text = "Output Readable";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // fileOpener
+            // 
+            this.fileOpener.DefaultExt = "*.json";
+            this.fileOpener.FileName = "openFileDialog1";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(13, 12);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(541, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(13, 51);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(751, 678);
+            this.txtOutput.TabIndex = 2;
+            this.txtOutput.WordWrap = false;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(561, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // ReadableJsonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 741);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.btnOutput);
+            this.Name = "ReadableJsonForm";
+            this.Text = "Readable Json";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.OpenFileDialog fileOpener;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnBrowse;
+    }
+}
+
---
+++ Scratch/ScratchForm/ReadableJsonForm.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScratchForm
+{
+    public partial class ReadableJsonForm : Form
+    {
+        public ReadableJsonForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if(fileOpener.ShowDialog() == DialogResult.OK)
+            {
+                this.txtFile.Text = fileOpener.FileName;
+            }
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(this.txtFile.Text.Trim()))
+                this.txtOutput.Text = ReadAndOutput(this.txtFile.Text.Trim());
+            else
+                MessageBox.Show("File is not existing!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private string ReadAndOutput(string file)
+        {
+            var obj = JsonConvert.DeserializeObject(File.ReadAllText(file));
+            return JsonConvert.SerializeObject(obj, Formatting.Indented);
+        }
+    }
+}
